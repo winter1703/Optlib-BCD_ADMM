1e1f44f5129862a9e377c9f304a32bbc96f31c6a
Chenyi Li <108876608+chenyili0818@users.noreply.github.com>
2024-07-22 09:45:07 +0800

    Update KL.lean

--- Convex/BCD/KL.lean
+++ Convex/BCD/KL.lean
@@ -13,6 +13,12 @@ variable {f : E → ℝ} {x : E}
 def special_concave (η : ℝ) := {f : ℝ → ℝ | (ConcaveOn ℝ (Ico 0 η) f) ∧ (∀ x ∈ Ico 0 η, f x > 0)
   ∧ (f 0 = 0) ∧ (ContDiffOn ℝ 1 f (Ioo 0 η)) ∧ (ContinuousAt f 0) ∧ (∀ x ∈ Ioo 0 η, deriv f x > 0)}
 
+/- KL property with specific φ -/
+def KL_point_with_reparameter (σ : E → ℝ) (u : E) (φ : ℝ → ℝ) : Prop :=
+  ∃ η ∈ Ioi 0, ∃ s ∈ 𝓝 u, (φ ∈ special_concave η) ∧  (∀ x ∈ s ∩
+  {y ∈ active_domain σ | σ u < σ y ∧ σ y < σ u + η},
+  deriv φ (σ x - σ u) * (EMetric.infEdist 0 (subdifferential σ x)).toReal ≥ 1)
+
 /- the definition of the KL property at one point -/
 def KL_point (σ : E → ℝ) (u : E) : Prop :=
   ∃ η ∈ Ioi 0, ∃ s ∈ 𝓝 u, ∃ φ ∈ special_concave η, ∀ x ∈ s ∩
